c5a015438ec3bdfa9d27b34f71c0c87920c3ae53
Siebe Joris Jochems <siebz0r@gmail.com>
2017-02-16 10:21:30 +0100

    docs: add missing docstrings
    Add docstrings for everything.

--- moby.py
+++ moby.py
@@ -1,3 +1,22 @@
+"""
+Moby is a tool to automate interaction with docker.
+
+Using a config file `moby.cfg`, moby automates interaction with docker.
+Files can be uploaded to and downloaded from a container and commands can be
+run in the container.
+
+Common args
+***********
+
+Most functions expect common args. They are documented here to prevent needless
+repetition.
+
+* client (.docker.APIClient): The docker client to use.
+* logger (.logging.Logger): The logger to use to log build info.
+
+
+"""
+
 import io
 import json
 import logging
@@ -14,6 +33,15 @@ BOLD = '\033[1m{}' + END
 
 
 def build_image(client, logger):
+    """
+    Build the docker image.
+
+    Build the docker image from the Dockerfile in the current directory.
+
+    Returns:
+        str: The id of the built image.
+
+    """
     logger.info(BOLD.format('Building image...\n'))
     image = client.build(
         path='.')
@@ -25,10 +53,31 @@ def build_image(client, logger):
 
 
 def init_client():
+    """
+    Initialise the docker client.
+
+    Returns:
+        .docker.APIClient: The docker client.
+
+    """
     return docker.APIClient()
 
 
 def init_logger(level=logging.INFO):
+    """
+    Initialise the logger.
+
+    Configure and initialise a logger. The logger is configured to log at the
+    given level. Newlines are omitted so no double newlines are logged.
+    The logger is configured to log to stdout.
+
+    Args:
+        level (int): The level the logger should log on.
+
+    Returns:
+        .logging.Logger: The initialised logger.
+
+    """
     logger = logging.getLogger(__name__)
     logger.setLevel(level)
 
@@ -45,12 +94,33 @@ def init_logger(level=logging.INFO):
 
 
 def load_config():
+    """
+    Load the moby config.
+
+    Load and parse the `moby.yml` config.
+
+    Returns:
+        dict: The parsed config.
+
+    """
     with open('moby.yml', 'r') as config:
         config = yaml.load(config)
     return config
 
 
 def pull(client, container, files, logger):
+    """
+    Pull files from the container.
+
+    Pull files from the container to the current directory of the host.
+    Filenames can be relative or absolute, if relative they are expected to be
+    relative to the current working dir of the container.
+
+    Args:
+        container (str): The id of the container.
+        files (list): A list of filenames (`str`) to download.
+
+    """
     cwd = run_command(client, container, 'pwd', logger, silent=True)
     for path in files:
         if not path.startswith('/'):
@@ -61,8 +131,17 @@ def pull(client, container, files, logger):
         archive.extractall()
 
 
-
 def push(client, container, files, logger):
+    """
+    Push files to the container.
+
+    Push files to the current working directory of the container.
+
+    Args:
+        container (str): The id of the container.
+        files (list): A list of filenames (`str`) to upload.
+
+    """
     cwd = run_command(client, container, 'pwd', logger, silent=True)
     archive_file = io.BytesIO()
     archive = tarfile.open(fileobj=archive_file, mode='w')
@@ -72,6 +151,24 @@ def push(client, container, files, logger):
 
 
 def run_command(client, container, command, logger, silent=False):
+    """
+    Run a command in a running container.
+
+    Args:
+        container (str): The id of the container.
+        command (str): The command to run.
+
+    Keyword Args:
+        silent (bool): Whether or not to suppress logging.
+
+    Returns:
+        str: The output of the command.
+
+    Raises:
+        SystemExit: When the command fails a SystemExit is raised with the
+            same exit code.
+
+    """
     if not silent:
         logger.info(BOLD.format('Running {!r}:\n'.format(command)))
     command = client.exec_create(
@@ -94,6 +191,22 @@ def run_command(client, container, command, logger, silent=False):
 
 
 def run_env(client, container, env, logger):
+    """
+    Run an environment.
+
+    Run the environment following this sequence of entries:
+
+    #. `before`
+    #. `push`
+    #. `run` **Required**
+    #. `pull`
+    #. `after`
+
+    Args:
+        container (str): The id of the container.
+        env (dict): The environment to run.
+
+    """
     if 'before' in env:
         run_env(client, container, env['before'], logger)
 
@@ -111,6 +224,18 @@ def run_env(client, container, env, logger):
 
 
 def start_container(client, image, logger):
+    """
+    Start a container.
+
+    Use an image to start a container.
+
+    Args:
+        image (str): The id of the image.
+
+    Returns:
+        str: The container id.
+
+    """
     logger.info(BOLD.format('Starting container...\n'))
     container = client.create_container(
         image,
@@ -122,11 +247,24 @@ def start_container(client, image, logger):
 
 
 def stop_container(client, container, logger):
+    """
+    Stop a running container.
+
+    Args:
+        container (str): The id of the container.
+
+    """
     logger.info(BOLD.format('Stopping container...\n'))
     client.stop(container)
 
 
 def main():
+    """
+    The main entry point of moby.
+
+    This function ties it all together.
+
+    """
     logger = init_logger()
     config = load_config()
     client = init_client()
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding utf-8
 
+"""Setup file for moby."""
+
 from setuptools import setup
 
 
